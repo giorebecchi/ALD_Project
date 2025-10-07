07f78dfb88467c1f6b2ffc5f5dc6dd3e99bf5b82
Paglia20 <145615930+Paglia20@users.noreply.github.com>
2025-10-07 18:36:12 +0200

    ALU

--- ALD_Project.srcs/sources_1/new/alu.vhd
+++ ALD_Project.srcs/sources_1/new/alu.vhd
@@ -1,46 +1,51 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 09/20/2025 03:35:45 PM
--- Design Name: 
--- Module Name: immgen - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 entity alu is
- --Port( 
-     
-   -- )
- 
-end alu;
+  generic (XLEN : integer := 32);
+  port (
+    a, b     : in  std_logic_vector(XLEN-1 downto 0);
+    op     : in  std_logic_vector(2 downto 0);
+    result   : out std_logic_vector(XLEN-1 downto 0);
+    zero     : out std_logic
+  );
+end entity;
 
-architecture Behavioral of alu is
-
+architecture rtl of alu is
+  constant ALU_OR                 : std_logic_vector(2 downto 0) := "000";
+  constant ALU_AND                : std_logic_vector(2 downto 0) := "001";
+  constant ALU_XOR                : std_logic_vector(2 downto 0) := "010";
+  constant ALU_UNUSED             : std_logic_vector(2 downto 0) := "011";
+  constant ALU_ADD                : std_logic_vector(2 downto 0) := "100";
+  constant ALU_SUB                : std_logic_vector(2 downto 0) := "101";
+  constant ALU_LESS_THAN_SIGNED   : std_logic_vector(2 downto 0) := "110";
+  constant ALU_LESS_THAN_UNSIGNED : std_logic_vector(2 downto 0) := "111";
 begin
-
-
-end Behavioral;
+  process(a, b, op)
+    variable tmp : std_logic_vector(XLEN-1 downto 0);
+  begin
+    case op is
+      when ALU_OR   => tmp := a or b;
+      when ALU_AND  => tmp := a and b;
+      when ALU_XOR  => tmp := a xor b;
+      when ALU_ADD  => tmp := std_logic_vector(signed(a) + signed(b));
+      when ALU_SUB  => tmp := std_logic_vector(signed(a) - signed(b));
+      when ALU_LESS_THAN_SIGNED =>
+        if signed(a) < signed(b) then
+          tmp := (others => '0'); tmp(0) := '1';
+        else
+          tmp := (others => '0');
+        end if;
+      when ALU_LESS_THAN_UNSIGNED =>
+        if unsigned(a) < unsigned(b) then
+          tmp := (others => '0'); tmp(0) := '1';
+        else
+          tmp := (others => '0');
+        end if;
+      when others => tmp := (others => '0');
+    end case;
+    result <= tmp;
+    zero   <= '1' when tmp = (others => '0') else '0';
+  end process;
+end architecture;
